9d9e3a3ef32ed848d1675a4d4206d372170b0c4c
X-Caboclean <146889041+xcaboclean@users.noreply.github.com>
2024-05-08 17:47:20 +0000

    Point scoring.

--- main.py
+++ main.py
@@ -10,10 +10,29 @@ def format_element(element):
   show_element = element['name'] + ", "+ element['description']+", from "+ element['country']+"."
   return show_element
 
+def check_answer(guess, account_a, account_b):
+
+  if account_a['follower_count'] > account_b['follower_count']:
+    bigguer = 'A'
+  elif account_a['follower_count'] < account_b['follower_count']:
+    bigguer = 'B'
+  return (guess == bigguer)
+
+
+score = 0  
 print(logo)
-account_a= format_element(select_element(data))
-account_b= format_element(select_element(data))
-print("Compare A: " + account_a)
+account_a= select_element(data)
+account_b= select_element(data)
+print("Compare A: " + format_element(account_a))
 print(vs)
-print("Against B: " + account_b)
-account_b= format_element(select_element(data))
+print("Against B: " + format_element(account_b))
+
+guess = input("Who more folowers? Type 'A' or 'B':")
+
+correct_guess = check_answer(guess, account_a, account_b) 
+
+if correct_guess:
+  score += 1
+  print(f"You're right! Current score: {score}.")
+else:
+  print(f"Sorry, that's wrong. Final score: {score}")

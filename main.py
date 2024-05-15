580a4e10cc864a6d211b634bee62e57016248393
X-Caboclean <146889041+xcaboclean@users.noreply.github.com>
2024-05-15 18:14:03 +0000

    fix - repeated account draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from art import logo, vs
 from game_data import data
 import random
+from replit import clear
 
 def select_element(data):
   element = random.choice(data)
@@ -19,20 +20,42 @@ def check_answer(guess, account_a, account_b):
   return (guess == bigguer)
 
 
+
+def game(account_a, account_b, score, keep_playing):
+  clear()
+  print(logo)
+  account_a= account_b
+  account_b= select_element(data) 
+  while account_a == account_b:
+    account_b= select_element(data) 
+    
+  print("Compare A: " + format_element(account_a))
+  print(vs)
+  print("Against B: " + format_element(account_b))
+
+  guess = input("Who more folowers? Type 'A' or 'B':")
+
+  correct_guess = check_answer(guess, account_a, account_b) 
+  print(correct_guess)
+  if correct_guess:
+    keep_playing = True
+    score += 1
+    message = f"You're right! Current score: {score}."
+  else:
+    keep_playing = False
+    message = f"Sorry, that's wrong. Final score: {score}"
+
+  print(message)
+  return score
+
+
+
+account_a= None
+account_b= None
+
 score = 0  
-print(logo)
-account_a= select_element(data)
 account_b= select_element(data)
-print("Compare A: " + format_element(account_a))
-print(vs)
-print("Against B: " + format_element(account_b))
-
-guess = input("Who more folowers? Type 'A' or 'B':")
-
-correct_guess = check_answer(guess, account_a, account_b) 
-
-if correct_guess:
-  score += 1
-  print(f"You're right! Current score: {score}.")
-else:
-  print(f"Sorry, that's wrong. Final score: {score}")
+keep_playing = True
+while keep_playing:
+  score = game(account_a, account_b, score, keep_playing)
+  
